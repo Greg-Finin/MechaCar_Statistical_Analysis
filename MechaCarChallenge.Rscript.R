c5d78c27a8027782fadfa23160272ac56c3ab5d2
Greg-Finin <gfinin@fordham.edu>
2021-01-14 10:28:48 -0500

    Update MechaCarChallenge.Rscript.R
    Addedd Suspension Data

--- MechaCarChallenge.Rscript.R
+++ MechaCarChallenge.Rscript.R
@@ -2,3 +2,7 @@ library(dplyr)
 Mechadata <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mechadata)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=Mechadata))
+
+SuspensionData <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+Total_Summary <- SuspensionData %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
+Lot_Summary <- SuspensionData %>% group_by(Manufacturing_Lot) %>% summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
